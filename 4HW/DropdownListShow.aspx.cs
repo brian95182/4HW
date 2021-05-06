ee189925d2051d2f7eb84b0e1a9b555fa556b40f
容喆 王 <107111203@mail.oit.edu.tw>
2021-05-07 01:59:51 +0800

    4126

--- 4HW/DropdownListShow.aspx.cs
+++ 4HW/DropdownListShow.aspx.cs
@@ -9,6 +9,39 @@ namespace _4HW {
     public partial class DropdownListShow : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
 
+            string[] place = { "基隆", "台北", "新北", "苗栗", "台中", "南投" };
+
+            if (dbl_Area.Items.Count == 0)
+            {
+                dbl_Area.Items.Add("北區");
+                dbl_Area.Items.Add("中區");
+                for (int p = 0; p < place.Length; p++)
+                {
+                    dbl_Place.Items.Add(place[p]);
+                }
+            }
+
+            if (dbl_Area.SelectedIndex == 0)
+            {
+                for (int p = 0; p < place.Length; p++)
+                {
+                    if (p < 3)
+                        dbl_Place.Items[p].Enabled = true;
+                    else
+                        dbl_Place.Items[p].Enabled = false;
+                }  
+                
+            }
+            else
+            {
+                for (int p = 0; p < place.Length; p++)
+                {
+                    if (p > 2)
+                        dbl_Place.Items[p].Enabled = true;
+                    else
+                        dbl_Place.Items[p].Enabled = false;
+                }
+            }
         }
     }
 }
